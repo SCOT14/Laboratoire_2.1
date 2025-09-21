d7a492adbaf29571748f9643ff64f5ec4b807e51
SCOT14 <Fluidity_tr@outlook.com>
2025-09-21 16:14:57 -0400

    ex2 incomplet (clignotement non fonctionnel)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+//ensemble des define et des librairies
 #include <Arduino.h>
 #include <bits_manip.h>
 #include <MOMO_RGB_Matrix.h>
@@ -12,7 +13,8 @@
 #define DEL3B PL2
 #define BTN PC2
 
-MOMO_RGB_Matrix matrix;
+
+MOMO_RGB_Matrix matrix; //création de la matrice
 const unsigned long temps_remp_rinc = 5000;
 const unsigned long clignote_lent = 500;
 const unsigned long clignote_vite = 250;
@@ -31,46 +33,47 @@ enum Etat {
 
 void setup() {
   // 
- Serial.begin(9600);
- matrix.begin();
- matrix.setCursor(8, 0);
- matrix.setTextColor(matrix.Color333(7,7,7));
- matrix.fillScreen(matrix.Color333(0, 0, 0));
+ Serial.begin(9600); //allumer le port série
+ matrix.begin(); //allume la matrice a del
+ matrix.setCursor(8, 0); //deplace le curseur au point 8,0
+ matrix.setTextColor(matrix.Color333(7,7,7)); //set la couleur des pixels de txt
+ matrix.fillScreen(matrix.Color333(0, 0, 0)); // remplis l'écran de noir 
  DDRG = B00000111; 
- DDRL = B11111100;
+ DDRL = B11111100;  //configuration entrée/sorties
  DDRC = B00000000;
  PORTC = B11111111; //active input pullup
 }
 
 
 
-
+static Etat etat = Attente; 
 
 void loop() {
   // put your main code here, to run repeatedly:
- Etat etat = Attente;
  unsigned long currentMillis = millis();
  static unsigned long etatStart = 0; 
-
+ if(PINC == B00000010){ //on effectue un arrêt si le bouton 2 est appuyer
+  etat = Attente; 
+ }
   switch(etat){
     case Attente:
-      matrix.fillScreen(matrix.Color333(0, 0, 0));
-      Serial.println("ARRET");
-      matrix.print("ARRET");
-      if(PINC = B00000001) //si le bouton est appuyer
-      etat = Remplissage;
+      matrix.fillScreen(matrix.Color333(0, 0, 0));  //noir
+      Serial.println("ARRET");  //affiche ARRET sur le port série
+      matrix.print("ARRET");    //affiche ARRET sur la matrice
+      if(PINC == B00000001) //si le bouton 1 est appuyer
+      etat = Remplissage; //change l'état de la machine
       break;
 
     case Remplissage:
       matrix.fillScreen(matrix.Color333(0, 0, 0));
       matrix.print("REMPLISSAGE");
       Serial.println("REMPLISSAGE");
-      PORTG = B00000010; //allume une del en vert 
-      if(currentMillis - etatStart >= temps_remp_rinc){
-        PORTG = B00000000;
-        etatStart = currentMillis;
+      analogWrite(PG1, 255); //allume une del en vert 
+      if(currentMillis - etatStart >= temps_remp_rinc){ //si le temps depasse 5000
+        analogWrite(PG1, 0); //on éteint la del
+        etatStart = currentMillis; //stock la variable
       }
-      etat = Lavage;
+      etat = Lavage;    //changement d'état
       break;
 
     case Lavage:
@@ -78,25 +81,25 @@ void loop() {
       matrix.print("LAVAGE");
       Serial.println("LAVAGE");
       for(int i = 0; i < 10; i++){
-        PORTL = B10000000;
-        if(currentMillis - etatStart >= clignote_lent){
-          PORTL = B00000000;
+        analogWrite(PG0, 255); //allume une del en bleu
+        if(currentMillis - etatStart >= clignote_lent){ //clignotement
+          analogWrite(PG0, 0);  //éteint la del
           etatStart = currentMillis;
         }
       }
-      etat = Rincage;
+      etat = Rincage;   //changement d'état
       break;
     
     case Rincage:
       matrix.fillScreen(matrix.Color333(0, 0, 0));
       matrix.print("RINCAGE");
       Serial.println("RINCAGE");
-      PORTL = B00000100;
+      analogWrite(PG2, 255);    //allume une del en rouge
       if(currentMillis - etatStart >=temps_remp_rinc){
-        PORTL = B00000000;
+        analogWrite(PG2, 0);    //éteint la del
         etatStart = currentMillis;
       }
-      etat = Essorage;
+      etat = Essorage;    //changement d'état
       break;
 
     case Essorage:
@@ -104,13 +107,13 @@ void loop() {
       matrix.print("ESSORAGE");
       Serial.println("ESSORAGE");
       for(int i = 0; i < 20; i++){
-        PORTL = B00000100;
+        analogWrite(PG2, 255);    //on fait clignoter la del 
         if(currentMillis - etatStart >= clignote_vite){
-          PORTL = B00000000;
+          analogWrite(PG2, 0);
           etatStart = currentMillis;
         }
       }
-      etat = Fin;
+      etat = Fin; //changement d'état
       break;
     
     case Fin:
@@ -118,7 +121,7 @@ void loop() {
       matrix.print("FIN CYCLE");
       Serial.println("FIN DU CYCLE");
       delay(1000);
-      etat = Attente;
+      etat = Attente; //retour au départ
       break;
 
   }
